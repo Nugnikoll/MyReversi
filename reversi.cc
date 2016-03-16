de623a67dc738b96404010034fc15ad9e5f1ff99
Rick <1300012743@pku.edu.cn>
2016-03-16 19:25:35 +0800

    The first stable version as a console program. A lot of changes have been made. Support tool command language. The depth of 7 + 1 can be reached with the algorithm of alpha-beta pruning. Some features of C++11 are used including lamda, random, chrono, enhanced for loop and auto. The function search can be debuged with macro DEBUG_SEARCH defined. Plan to accelerate it with unordered_map (hash table).

--- reversi.cc
+++ reversi.cc
@@ -1,6 +1,300 @@
+#include <vector>
+#include <algorithm>
+
 #include "reversi.h"
 
-board::calc_type board::table_count[board::enum_num];
-board::calc_type board::table_eval[board::stage_num][board::pos_num][board::enum_num];
-unordered_map<board::lline_type,board::line_type> board::table_valid;
-unordered_map<board::exline_type,board::lline_type> board::table_flip;
+const brd_type board::last = 0x8000000000000000;
+
+const char board::chr_print[board::chessman_num] = {'.','O','#','*'};
+calc_type board::table_param[stage_num][board::pos_num] = {{20,1,-6,-1},{10,2,-3,0},{4,4,4,4}};
+calc_type board::table_count[board::enum_num];
+calc_type board::table_eval[board::stage_num][board::layer_num][board::enum_num];
+
+board& board::mirror(cbool is_horizontal){
+	brd_type mask = 1,filter;
+	brd_type new_black = 0,new_white = 0;
+	if(is_horizontal){
+		for(int i = 0;i != size;++i){
+			for(int j = 0;j != size;++j){
+				filter = brd_type(1) << ((i << 3) + (size - j - 1));
+				if(filter & brd_black){
+					new_black |= mask;
+				}
+				if(filter & brd_white){
+					new_white |= mask;
+				}
+				mask <<= 1;
+			}
+		}
+	}else{
+		for(int i = 0;i != size;++i){
+			for(int j = 0;j != size;++j){
+				filter = brd_type(1) << (((size - i - 1) << 3) + j);
+				if(filter & brd_black){
+					new_black |= mask;
+				}
+				if(filter & brd_white){
+					new_white |= mask;
+				}
+				mask <<= 1;
+			}
+		}
+	}
+	brd_black = new_black;
+	brd_white = new_white;
+	return *this;
+}
+
+board& board::rotater(pos_type n90){
+	n90 = n90 % 4;
+	brd_type mask = 1,filter;
+	brd_type new_black = 0,new_white = 0;
+	if(n90 < 0){
+		n90 += 4;
+	}
+	if(n90 == 0){
+		return *this;
+	}else if(n90 == 1){
+		for(int i = 0;i != size;++i){
+			for(int j = 0;j != size;++j){
+				filter = brd_type(1) << (((size - j - 1) << 3) + i);
+				if(filter & brd_black){
+					new_black |= mask;
+				}
+				if(filter & brd_white){
+					new_white |= mask;
+				}
+				mask <<= 1;
+			}
+		}
+	}else if(n90 == 2){
+		filter = brd_type(1) << (size * size - 1);
+		for(int i = 0;i != size * size;++i){
+			if(filter & brd_black){
+				new_black |= mask;
+			}
+			if(filter & brd_white){
+				new_white |= mask;
+			}
+			mask <<= 1;
+			filter >>= 1;
+		}
+	}else if(n90 == 3){
+		for(int i = 0;i != size;++i){
+			for(int j = 0;j != size;++j){
+				filter = brd_type(1) << ((j << 3) + (size - i - 1));
+				if(filter & brd_black){
+					new_black |= mask;
+				}
+				if(filter & brd_white){
+					new_white |= mask;
+				}
+				mask <<= 1;
+			}
+		}
+	}
+	brd_black = new_black;
+	brd_white = new_white;
+	return *this;
+}
+
+void board::config(){
+	calc_type value;
+	pos_type mask;
+	for(pos_type i = 0;i != stage_num;++i){
+		for(pos_type j = 0;j != size;++j){
+			for(pos_type k = 0;k != enum_num;++k){
+				value = 0;
+				for(pos_type l = 0;l != size;++l){
+					mask = 1 << l;
+					if(mask & k){
+						if(j == 0 && (l == 0 || l == size - 1)){
+							value += table_param[i][0];
+						}else if(j == 0 || l == 0 || l == size - 1){
+							value += table_param[i][1];
+						}else if(j == 1 && (l == 1 || l == size - 2)){
+							value += table_param[i][2];
+						}else{
+							value += table_param[i][3];
+						}
+					}
+				}
+				table_eval[i][j][k] = value;
+			}
+		}
+	}
+	for(pos_type k = 0;k != enum_num;++k){
+		value = 0;
+		for(pos_type l = 0;l != size;++l){
+			mask = 1 << l;
+			if(mask & k){
+				++value;
+			}
+		}
+		table_count[k] = value;
+	}
+}
+
+calc_type board::search(cbool color,cshort height,calc_type alpha,
+	ccalc_type beta,calc_type acc,cpos_type stage)const{
+	conf_score conf = stage_config(stage);
+	if(color){
+		return search<true>(height,alpha,beta,acc,conf);
+	}else{
+		return search<false>(height,alpha,beta,acc,conf);
+	}
+}
+vector<board::choice> board::get_choices(cbool color,cshort height,cpos_type stage)const{
+	conf_score conf = stage_config(stage);
+	if(color){
+		return get_choices<true>(height,conf);
+	}else{
+		return get_choices<false>(height,conf);
+	}
+}
+
+template<bool color>
+calc_type board::search(cshort height,calc_type alpha,calc_type beta,calc_type acc,cconf_score conf)const{
+
+	#ifdef DEBUG_SEARCH
+	auto fun = [&]()->calc_type{
+	#endif
+
+	if(height == 0){
+		return this->score<color>(conf) + acc;
+	}
+
+	vector<board> vec;
+	calc_type temp;
+	
+	vec.reserve(30);
+	vec.push_back(*this);
+	for(register brd_type i = 1;i != last;i <<= 1){
+		if(vec.back().flip<color>(i)){
+			vec.push_back(*this);
+		}
+	}
+	if(!vec.back().flip<color>(last)){
+		vec.pop_back();
+	}
+	
+	if(vec.empty()){
+		vec.push_back(*this);
+		for(register brd_type i = 1;i != last;i <<= 1){
+			if(vec.back().flip<!color>(i)){
+				vec.push_back(*this);
+			}
+		}
+		if(!vec.back().flip<!color>(last)){
+			vec.pop_back();
+		}
+
+		if(vec.empty()){
+			calc_type num_diff = count<color>() - count<!color>();
+			num_diff <<= 4;
+			if(num_diff){
+				if(num_diff > 0){
+					return num_diff + mark_max;
+				}else if(num_diff < 0){
+					return num_diff - mark_max;
+				}else{
+					return num_diff;
+				}
+			}else{
+				return 0;
+			}
+		}else{
+			acc = (acc >> 1) - vec.size();
+			for(const auto& brd:vec){
+				temp = brd.search<color>(height - 1,alpha,beta,acc,conf);
+				if(temp <= alpha)
+					return alpha;
+				if(temp < beta)
+					beta = temp;
+			}
+			return beta;
+		}
+	}else{
+		acc = vec.size() + (acc >> 1);
+		for(const auto& brd:vec){
+			temp = - brd.search<!color>(height - 1,-beta,-alpha,-acc,conf);
+			if(temp >= beta)
+				return beta;
+			if(temp > alpha)
+				alpha = temp;
+		}
+	}
+    return alpha;
+
+	#ifdef DEBUG_SEARCH
+	};
+	out << "<div color=" << color
+		<<" height=" << height
+		<< " alpha=" << alpha
+		<< " beta=" << beta
+		<< " acc=" << acc
+		<< ">\n";
+	do_print(out);
+	calc_type result = fun();
+	out << "result = " << result <<"\n"
+		<< "</div>\n";
+	return result;
+	#endif
+}
+
+template<bool color>
+vector<board::choice> board::get_choices(cshort height,board::cconf_score conf)const{
+
+    vector<choice> choices;
+	calc_type result;
+    choice temp;
+	calc_type alpha = _inf;
+
+    if(height < 0)
+        return choices;
+
+    board brd = *this;
+	for(pos_type i = 0;i != size;++i){
+		for(pos_type j = 0;j != size;++j){
+			if(brd.flip<color>(brd_type(1) << ((j << 3) + i))){
+				result = - brd.search<!color>(height,_inf,-alpha,0,conf);
+				if(result - 5 > alpha){
+					alpha = result - 5;
+				}
+				std::get<0>(temp) = result;
+				std::get<1>(temp) = brd;
+				std::get<2>(temp) = i;
+				std::get<3>(temp) = j;
+				choices.push_back(temp);
+				brd = *this;
+			}
+		}
+	}
+    return choices;
+}
+
+#include <random>
+#include <chrono>
+default_random_engine engine(
+	chrono::system_clock::now().time_since_epoch().count()
+);
+
+board::choice board::select_choice(vector<board::choice> choices)const{
+
+	if(choices.empty()){
+		throw runtime_error("There's no choice!");
+	}
+
+	normal_distribution<float> scatter(0,0.8);
+
+	for(choice& c:choices){
+		std::get<0>(c) += scatter(engine);
+	}
+
+	return *max_element(
+		choices.begin(),choices.end(),
+		[](const choice& c1,const choice& c2) -> bool{
+			return std::get<0>(c1) < std::get<0>(c2);
+		}
+	);
+}
--- reversi.h
+++ reversi.h
@@ -1,40 +1,63 @@
 #include <iostream>
-#include <unordered_map>
+#include <limits>
+#include <tuple>
+#include <vector>
 
 using namespace std;
 
+//#define DEBUG_SEARCH
+
+#ifdef DEBUG_SEARCH
+	#include <fstream>
+	extern ofstream out;
+#endif
+
+extern unsigned seed;
+
+typedef const bool& cbool;
+typedef const short& cshort;
+typedef const int& cint;
+
+typedef unsigned long long brd_type;
+typedef const brd_type& cbrd_type;
+typedef unsigned char line_type;
+typedef const line_type& cline_type;
+typedef short pos_type;
+typedef const pos_type& cpos_type;
+typedef short calc_type;
+typedef const calc_type& ccalc_type;
+enum chessman{blank,white,black,null};
+typedef const chessman& cchessman;
+
+const calc_type inf = numeric_limits<short>::max();
+const calc_type _inf = - inf;
+
 class board{
+protected:
+	struct conf_score{
+		calc_type *a,*b,*c;
+	};
+	typedef const conf_score& cconf_score;
 public:
-	typedef unsigned long long brd_type;
-	typedef unsigned char line_type;
-	typedef unsigned short lline_type;
-	typedef unsigned int exline_type;
-	typedef short pos_type;
-	typedef short calc_type;
 
 	board(){};
-	board(const brd_type& _brd_black,const brd_type& _brd_white)
+	board(cbrd_type _brd_black,cbrd_type _brd_white)
 		:brd_black(_brd_black),brd_white(_brd_white){}
 
-	template<bool color> const brd_type& get()const;
-	template<bool color> brd_type& get();
-	const brd_type& visit(const bool& color)const{
-		return color ? get<true>() : get<false>();
-	}
-	brd_type& visit(const bool& color){
-		return color ? get<true>() : get<false>();
-	}
-
-	enum chessman{blank,white,black,null};
+	static const brd_type last;
 	static const pos_type chessman_num = 4;
-	static const pos_type size = 4;
-	static const brd_type begin = 0x0;
-	static const brd_type end = 0x8000000000000000;
-	constexpr static const char chr_print[chessman_num] = {'.','#','O','*'};
-	enum pos_line{pos_edge,pos_margin,pos_center};
-	static const pos_type pos_num = pos_center;
-	static const pos_type stage_num = 5;
+	static const pos_type size = 8;
+	static const pos_type size2 = size * size;
+	static const char chr_print[chessman_num];
+	enum pos_enum{pos_angle,pos_edge,pos_inner,pos_center};
+	static const pos_type pos_num = 4;
+	static const pos_type layer_num = 3;
+	static const pos_type stage_num = 3;
 	static const pos_type enum_num = 256;
+
+	static calc_type table_param[stage_num][pos_num];
+
+	typedef tuple<float,board,pos_type,pos_type> choice;
 
 	friend ostream& operator<<(ostream& out,const board& brd){
 		brd.do_print(out);
@@ -48,27 +71,171 @@ public:
 		return *this;
 	}
 
+	static void config();
+	static conf_score stage_config(cpos_type stage){
+		conf_score conf;
+		if(stage < 0 || stage >= stage_num){
+			cout << "Error: out of range\n";
+			conf.a = conf.b = conf.c = 0;
+			return conf;
+		}
+		static bool flag = true;
+		if(flag){
+			config();
+			flag = false;
+		}
+		conf.a = table_eval[stage][0];
+		conf.b = table_eval[stage][1];
+		conf.c = table_eval[stage][2];
+		return conf;
+	}
+
+	board& assign(cbrd_type _brd_black,cbrd_type _brd_white){
+		brd_black = _brd_black;
+		brd_white = _brd_white;
+		return *this;
+	}
+	brd_type bget(cbool color)const{
+		if(color){
+			return this->bget<true>();
+		}else{
+			return this->bget<false>();
+		}
+	}
+	chessman get(cpos_type x, cpos_type y)const{
+		if(x < 0 || y < 0 || x >= size || y >= size){
+			cout << "Error: out of range\n";
+			return blank;
+		}
+		return get(brd_type(1) << ((y << 3) + x));
+	}
+
+	board& set(cpos_type x, cpos_type y, cchessman chsman){
+		if(x < 0 || y < 0 || x >= size || y >= size){
+			cout << "Error: out of range\n";
+			return *this;
+		}
+		return set(brd_type(1) << ((y << 3) + x),chsman);
+	}
+
+	board& mirror(cbool is_horizontal);
+	board& rotater(pos_type n90);
+	board& rotatel(pos_type n90){
+		return rotater(4 - n90);
+	}
+
+	bool flip(cbool color, cpos_type x, cpos_type y){
+		if(x < 0 || y < 0 || x >= size || y >= size){
+			cout << "Error: out of range\n";
+			return false;
+		}
+		if(color){
+			return flip<true>(brd_type(1) << ((y << 3) + x));
+		}else{
+			return flip<false>(brd_type(1) << ((y << 3) + x));
+		}
+	}
+	calc_type count(cbool color)const{
+		if(color){
+			return count<true>();
+		}else{
+			return count<false>();
+		}
+	}
+	calc_type sum()const{
+		return count<true>() + count<false>();
+	} 
+	calc_type score(cbool color,cpos_type stage)const{
+		conf_score conf = stage_config(stage);
+		if(color){
+			return score<true>(conf);
+		}else{
+			return score<false>(conf);
+		}
+	}
+	calc_type search(cbool color,cshort height,cpos_type stage)const{
+		return search(color,height,_inf,inf,0,stage);
+	}
+	calc_type search(cbool color,cshort height,calc_type alpha,
+		ccalc_type beta,calc_type acc,cpos_type stage)const;
+	vector<choice> get_choices(cbool color,cshort height,cpos_type stage)const;
+	choice select_choice(vector<choice> choices)const;
+	tuple<pos_type,pos_type> play(cbool color){
+		short height;
+		pos_type stage;
+		short total = this->sum();
+		if(total <= 33){
+			height = 7; stage = 0;
+		}else if(total <= size2 - 15){
+			height = 7; stage = 1;
+		}else{
+			height = 20; stage = 2;
+		}
+		vector<choice> choices = get_choices(color,height,stage);
+		if(choices.empty()){
+			return tuple<pos_type,pos_type>(-1,-1);
+		}else{
+			choice best = select_choice(choices);
+			flip(color,std::get<2>(best),std::get<3>(best));
+			return tuple<pos_type,pos_type>(std::get<2>(best),std::get<3>(best));
+		}
+	}
+
 protected:
 
 	brd_type brd_black,brd_white;
 
+	static const brd_type  lbound = 0xfefefefefefefefe;
+	static const brd_type  rbound = 0x7f7f7f7f7f7f7f7f;
+	static const brd_type  ubound = 0xffffffffffffff00;
+	static const brd_type  dbound = 0x00ffffffffffffff;
+	static const brd_type ulbound = 0xfefefefefefefe00;
+	static const brd_type urbound = 0x7f7f7f7f7f7f7f00;
+	static const brd_type dlbound = 0x00fefefefefefefe;
+	static const brd_type drbound = 0x007f7f7f7f7f7f7f;
+
 	static calc_type table_count[enum_num];
-	static calc_type table_eval[stage_num][board::pos_num][enum_num];
-	static unordered_map<lline_type,line_type> table_valid;
-	static unordered_map<exline_type,lline_type> table_flip; 
+	static calc_type table_eval[stage_num][layer_num][enum_num];
+
+	static void up(brd_type& mask){mask >>= 8;}
+	static void down(brd_type& mask){mask <<= 8;}
+	static void left(brd_type& mask){mask >>= 1;}
+	static void right(brd_type& mask){mask <<= 1;}
+	static void uleft(brd_type& mask){mask >>= 9;}
+	static void uright(brd_type& mask){mask >>= 7;}
+	static void dleft(brd_type& mask){mask <<= 7;}
+	static void dright(brd_type& mask){mask <<= 9;}
 
 	const board& do_print(ostream& out = cout)const{
 		brd_type mask = 1;
 		for(pos_type i = 0;i != size;++i){
 			for(pos_type j = 0;j != size;++j){
-				out << mask_visit(mask);
+				out << chr_print[get(mask)];
 				mask <<= 1;
 			}
-			cout << '\n';
+			out << '\n';
 		}
 		return *this;
 	}
-	chessman mask_visit(const pos_type& mask)const{
+
+	template<bool color> inline
+	brd_type& bget(){
+		if(color){
+			return brd_black;
+		}else{
+			return brd_white;
+		}
+	}
+	template<bool color> inline
+	const brd_type& bget()const{
+		if(color){
+			return brd_black;
+		}else{
+			return brd_white;
+		}
+	}
+
+	inline chessman get(cbrd_type mask)const{
 		if(mask & brd_black){
 			if(mask & brd_white){
 				return null;
@@ -83,17 +250,8 @@ protected:
 			}
 		}
 	}
-	template<bool color>
-	board& mask_set(const brd_type& mask,bool flag){
-		brd_type& handle = this->get<color>();
-		if(flag){
-			handle |= mask;
-		}else{
-			handle &= ~mask;
-		}
-		return *this;
-	}
-	board& mask_set(const brd_type& mask,bool color,bool flag){
+	template<bool color> inline
+	board& set(cbrd_type mask, cbool flag){
 		if(color){
 			if(flag){
 				brd_black |= mask;
@@ -109,7 +267,7 @@ protected:
 		}
 		return *this;
 	}
-	board& mask_set(const brd_type& mask,chessman chsm){
+	inline board& set(cbrd_type mask, cchessman chsm){
 		if(chsm & white){
 			brd_white |= mask;
 		}else{
@@ -122,17 +280,187 @@ protected:
 		}
 		return *this;
 	}
-};
 
-template<> inline const board::brd_type& board::get<true>()const{
-	return brd_black;
-}
-template<> inline const board::brd_type& board::get<false>()const{
-	return brd_white;
-}
-template<> inline board::brd_type& board::get<true>(){
-	return brd_black;
-}
-template<> inline board::brd_type& board::get<false>(){
-	return brd_white;
-}
+	template<bool color>
+	bool flip(cbrd_type mask){
+		if((brd_black | brd_white) & mask){
+			return false;
+		}
+		bool everflip = false;
+		brd_type pos = mask;
+		brd_type& blue = bget<color>();
+		brd_type& green = bget<!color>();
+		while(pos & lbound){
+			left(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(right(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & rbound){
+			right(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(left(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & ubound){
+			up(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(down(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & dbound){
+			down(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(up(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & ulbound){
+			uleft(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(dright(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & urbound){
+			uright(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(dleft(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & dlbound){
+			dleft(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(uright(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		pos = mask;
+		while(pos & drbound){
+			dright(pos);
+			if(green & pos)
+				continue;
+			if(blue & pos){
+				while(uleft(pos), pos != mask){
+					blue |= pos;
+					green &= ~pos;
+					everflip = true;
+				}
+			}
+			break;
+		}
+		if(everflip){
+			blue |= mask;
+			green &= ~mask;
+		}
+		return everflip;
+	}
+
+	template<bool color>
+	calc_type count()const{
+
+		brd_type blue = bget<color>();
+		calc_type result;
+
+		result = table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue]; blue >>= size;
+		result += table_count[(unsigned char&)blue];
+
+		return result;
+	}
+	template<bool color> inline
+	calc_type score(cconf_score conf)const{
+
+		brd_type blue = bget<color>();
+		brd_type green = bget<!color>();
+		calc_type result;
+
+		result = conf.a[(unsigned char&)blue]; blue >>= size;
+		result += conf.b[(unsigned char&)blue]; blue >>= size;
+		result += conf.c[(unsigned char&)blue]; blue >>= size;
+		result += conf.c[(unsigned char&)blue]; blue >>= size;
+		result += conf.c[(unsigned char&)blue]; blue >>= size;
+		result += conf.c[(unsigned char&)blue]; blue >>= size;
+		result += conf.b[(unsigned char&)blue]; blue >>= size;
+		result += conf.a[(unsigned char&)blue];
+
+		result -= conf.a[(unsigned char&)green]; green >>= size;
+		result -= conf.b[(unsigned char&)green]; green >>= size;
+		result -= conf.c[(unsigned char&)green]; green >>= size;
+		result -= conf.c[(unsigned char&)green]; green >>= size;
+		result -= conf.c[(unsigned char&)green]; green >>= size;
+		result -= conf.c[(unsigned char&)green]; green >>= size;
+		result -= conf.b[(unsigned char&)green]; green >>= size;
+		result -= conf.a[(unsigned char&)green];
+
+		return result;
+	}
+
+	static const calc_type mark_max = 10000;
+
+	template<bool color>
+	calc_type search(cshort height,cconf_score conf)const{
+		return search<color>(height,_inf,inf,0,conf);
+	}
+	template<bool color>
+	calc_type search(cshort height,calc_type alpha,calc_type beta,calc_type acc,cconf_score conf)const;
+	template<bool color>
+	vector<choice> get_choices(cshort height,cconf_score conf)const;
+};
