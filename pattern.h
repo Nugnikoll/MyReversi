01bd73d81ca0229c9aa2e3830ae0830635c4e334
Rick <1300012743@pku.edu.cn>
2016-11-18 11:17:17 +0800

    Support class pattern and class group in tcl.
    Signed-off-by: Rick <1300012743@pku.edu.cn>

--- pattern.h
+++ pattern.h
@@ -18,6 +18,9 @@ struct pattern{
 	float table[size][length];
 
 	void initial();
+	float get(const int& num,const int& pos){
+		return table[num][pos];
+	};
 	void compress(float* const& ptr);
 	void decompress(float* const& ptr);
 };
@@ -26,12 +29,19 @@ class group{
 public:
 	vector<pattern> vec;
 	vector<short> record;
-	void assign(const size_t& size);
-	void initial();
+	void assign(const int& size);
+	void initial(){
+		for(auto& ptn:vec){
+			ptn.initial();
+		}
+	}
 	void load(
 		const string& filename,cbool is_compress = true,cint num_begin = 0,cint num = 100
 		,cbool is_compatible = true);
 	void save(const string& filename,const bool& is_compress = true);
+	pattern* get(const int& pos){
+		return &vec.at(pos);
+	}
 	void train();
 	void print_record();
 };
@@ -41,5 +51,7 @@ bool compete(pattern* const& p1,pattern* const& p2);
 bool is_prime(const long long& num);
 
 extern pattern* ptr_pattern;
+
+extern void set_ptn(pattern* ptr);
 
 #endif //PATTERN_H
