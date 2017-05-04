805053c14a3ec78768bc8c64fb4bcd879cbf2b75
Rick <1300012743@pku.edu.cn>
2017-05-04 16:24:40 +0800

    Find a bug in function score_ptn but cannot fix it completely.

--- python/pattern_train.py
+++ python/pattern_train.py
@@ -6,13 +6,13 @@ grp.assign(10);
 for i in range(1000):
 	grp.train();
 
-grp.save("pattern_g10_i1000_2.dat");
+grp.save("ptn_d0g10i1000.dat");
 
-print("pattern 1000");
+print("ptn_d0g10i1000");
 
 for i in range(10000):
 	grp.train();
 
-grp.save("pattern_g10_i10000_2.dat");
+grp.save("ptn_d0g10i10000.dat");
 
-print("pattern 10000");
+print("ptn_d0g10i10000");
