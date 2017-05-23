d7a5645dd0a89e74845d5831b653376e7780238f
Rick <1300012743@pku.edu.cn>
2017-05-23 13:45:25 +0800

    Modify the algorithm to extract the pattern.
    Note: instruction emms must be added after the use of register mmx in case of NAN.
    
    Former-commit-id: ee3bf864e5f4930b176757cfa252efa2878ac714

--- test.tcl
+++ test.tcl
@@ -1,18 +1,3 @@
-set grp [group]
-# $grp assign 2
-# $grp initial
-
-# for {set i 0} {$i < 10} {incr i} {
-	# for {set j 0} {$j < 1000} {incr j} {$grp train}
-	# set ptn [$grp get 0]
-# }
-
-$grp load "python/pattern_100.dat" 1 0 100 1
-
-set ptn [$grp get 0]
-set_ptn $ptn
-
+grp_load "python/ptn_d0g10i10000.dat" 0 100
+use_ptn 0
 set mthd_default $mthd_ptn
-
-#puts [[[$grp get 0] get2 0 0] to_str]
-#play $mthd_ptn $true 7 -1; play $mthd_ab $false 7 -1;
