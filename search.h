5d672bf0f9dd31b9172fc3061c5565af147993ba
Rick <1300012743@pku.edu.cn>
2017-07-05 18:09:16 +0800

    Initial table_val[] in the function get_choice .

--- search.h
+++ search.h
@@ -2,6 +2,8 @@
 #define SEARCH_H
 
 typedef unordered_map<board,board::interval> trans_type;
+extern calc_type table_val[board::size2][board::size2];
+extern const calc_type table_val_init[board::size2];
 extern trans_type table_trans;
 
 #endif //SEARCH_H
