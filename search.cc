71b61b97a9b098241a39a22eca1ba65b066e40d5
Rick <1300012743@pku.edu.cn>
2017-07-14 14:38:58 +0800

    Remove the array mask_after in search.cc .
    Former-commit-id: 08fabfdbd791d79003ca8daed26252f5d24aee9c

--- search.cc
+++ search.cc
@@ -208,73 +208,6 @@ calc_type board::search(
 	#pragma GCC diagnostic pop
 #endif
 
-const brd_type mask_after[board::size2] = {
-	0xfffffffffffffffe,
-	0xfffffffffffffffc,
-	0xfffffffffffffff8,
-	0xfffffffffffffff0,
-	0xffffffffffffffe0,
-	0xffffffffffffffc0,
-	0xffffffffffffff80,
-	0xffffffffffffff00,
-	0xfffffffffffffe00,
-	0xfffffffffffffc00,
-	0xfffffffffffff800,
-	0xfffffffffffff000,
-	0xffffffffffffe000,
-	0xffffffffffffc000,
-	0xffffffffffff8000,
-	0xffffffffffff0000,
-	0xfffffffffffe0000,
-	0xfffffffffffc0000,
-	0xfffffffffff80000,
-	0xfffffffffff00000,
-	0xffffffffffe00000,
-	0xffffffffffc00000,
-	0xffffffffff800000,
-	0xffffffffff000000,
-	0xfffffffffe000000,
-	0xfffffffffc000000,
-	0xfffffffff8000000,
-	0xfffffffff0000000,
-	0xffffffffe0000000,
-	0xffffffffc0000000,
-	0xffffffff80000000,
-	0xffffffff00000000,
-	0xfffffffe00000000,
-	0xfffffffc00000000,
-	0xfffffff800000000,
-	0xfffffff000000000,
-	0xffffffe000000000,
-	0xffffffc000000000,
-	0xffffff8000000000,
-	0xffffff0000000000,
-	0xfffffe0000000000,
-	0xfffffc0000000000,
-	0xfffff80000000000,
-	0xfffff00000000000,
-	0xffffe00000000000,
-	0xffffc00000000000,
-	0xffff800000000000,
-	0xffff000000000000,
-	0xfffe000000000000,
-	0xfffc000000000000,
-	0xfff8000000000000,
-	0xfff0000000000000,
-	0xffe0000000000000,
-	0xffc0000000000000,
-	0xff80000000000000,
-	0xff00000000000000,
-	0xfe00000000000000,
-	0xfc00000000000000,
-	0xf800000000000000,
-	0xf000000000000000,
-	0xe000000000000000,
-	0xc000000000000000,
-	0x8000000000000000,
-	0x0000000000000000
-};
-
 template<method mthd>
 calc_type board::search(cbool color,cshort height,calc_type alpha,calc_type beta)const{
 
@@ -393,10 +326,11 @@ calc_type board::search(cbool color,cshort height,calc_type alpha,calc_type beta
 			}
 			++ptr;
 			ptr->brd = *this;
+			brd_move &= brd_move - 1;
 			asm volatile(
 				"tzcnt %1, %0;"
 				:"=&r"(pos)
-				:"r"(brd_move & mask_after[pos])
+				:"r"(brd_move)
 				:
 			);
 		}
@@ -469,10 +403,11 @@ calc_type board::search(cbool color,cshort height,calc_type alpha,calc_type beta
 				}
 				++ptr;
 				ptr->brd = *this;
+				brd_move &= brd_move - 1;
 				asm volatile(
 					"tzcnt %1, %0;"
 					:"=&r"(pos)
-					:"r"(brd_move & mask_after[pos])
+					:"r"(brd_move)
 					:
 				);
 			}
