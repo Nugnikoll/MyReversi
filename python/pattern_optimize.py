36e602bf614db4cb3e6de142755fc372ce336ba6
Rick <1300012743@pku.edu.cn>
2018-06-25 10:01:34 +0800

    Modify pattern_optimize.py . "sample.dat" and "target.dat" will not be generated when they are exist.
    Former-commit-id: 3a2237d0ce203ddcf442bed4f738b9373cc85c7c

--- python/pattern_optimize.py
+++ python/pattern_optimize.py
@@ -1,12 +1,14 @@
-import reversi as rv
-import time
-import matplotlib.pyplot as plt
+import reversi as rv;
+import time;
+import matplotlib.pyplot as plt;
+import os;
 
+mthd = rv.mthd_ab | rv.mthd_kill | rv.mthd_pvs | rv.mthd_trans | rv.mthd_mtdf | rv.mthd_ptn;
 dir_save = "./";
 
 rv.board.config();
 rv.pattern.config();
-rv.group.config("ptn_opt.dat");
+rv.group.config("../data/pattern.dat");
 
 ptn = rv.pattern();
 ptn.initial();
@@ -14,26 +16,41 @@ ptn.initial();
 size = 100000;
 alpha = 0.004 / size;
 
-time1 = time.clock();
-sample = rv.sample_gen(size);
-time2 = time.clock();
-print("time1",time2 - time1);
-sample = rv.sample_2mat(sample);
-time3 = time.clock();
-print("time2",time3 - time2);
-print(sample.geth(),sample.getw());
-target = rv.evaluate(sample,rv.mthd_default & ~rv.mthd_ptn & ~rv.mthd_trans ,4);
-time4 = time.clock();
-print("time3",time4 - time3);
+name = "sample.dat";
+if os.path.exists(name):
+	sample = rv.mat_brd(size, 1);
+	sample.load(name);
+else:
+	print("generate sample");
+	time1 = time.clock();
+	sample = rv.sample_gen(size);
+	time2 = time.clock();
+	print("time1",time2 - time1);
+	sample = rv.sample_2mat(sample);
+	time3 = time.clock();
+	print("time2",time3 - time2);
+	sample.save(name);
 
-sample.save(dir_save + "sample.dat");
-target.save(dir_save + "target.dat");
+print(sample.geth(),sample.getw());
+
+name = "target.dat";
+if os.path.exists(name):
+	target = rv.mat_f(size, 1);
+	target.load(name);
+else:
+	print("generate target");
+	time3 = time.clock();
+	target = rv.evaluate(sample, mthd & ~rv.mthd_ptn & ~rv.mthd_trans ,4);
+	time4 = time.clock();
+	print("time3",time4 - time3);
+	target.save(name);
 
 value = rv.evaluate(ptn,sample);
 epsilon = target - value;
 epsilon_2 = epsilon.modulus();
 
-epsilon_save = rv.mat_f(epsilon);
+print(epsilon.geth(), epsilon.getw())
+epsilon_save = rv.mat_f(epsilon);print("okok")
 ptn_save = rv.pattern(ptn)
 
 epsilon = rv.mat_f(epsilon_save);
