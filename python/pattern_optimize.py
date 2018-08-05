1ffc063b0b3490f37b4b429a2e5914fef6fd967a
Rick <1300012743@pku.edu.cn>
2018-08-05 12:20:06 +0800

    Try to use L-BFGS to optimize patterns.

--- python/pattern_optimize.py
+++ python/pattern_optimize.py
@@ -2,6 +2,8 @@ import reversi as rv;
 import time;
 import matplotlib.pyplot as plt;
 import os;
+import numpy as np;
+from scipy import optimize;
 
 mthd = rv.mthd_ab | rv.mthd_kill | rv.mthd_pvs | rv.mthd_trans | rv.mthd_mtdf | rv.mthd_ptn;
 dir_save = "./";
@@ -24,7 +26,7 @@ ptn.initial();
 
 size = 100000;
 #alpha = 0.004 / size;
-alpha = 0.004 / size * 50;
+#alpha = 0.004 / size * 50;
 
 name = "sample.dat";
 if os.path.exists(name):
@@ -41,6 +43,9 @@ else:
 	print("time2:",time3 - time2);
 	sample.save(name);
 
+size = sample.geth();
+alpha = 0.004 / size;
+
 print("sample size: ", sample.geth(), sample.getw());
 
 name = "target.dat";
@@ -55,30 +60,70 @@ else:
 	print("time3:",time4 - time3);
 	target.save(name);
 
-value = rv.evaluate(ptn,sample);
-epsilon = target - value;
-epsilon_2 = epsilon.modulus();
+print("shape:", ptn.numpy().shape);
+weight = ptn.numpy().ravel();
+print(weight.shape)
+def fun(weight):
+	value = rv.evaluate(ptn, sample);
+	epsilon = value - target;
+	eps = epsilon.numpy().astype(np.float64);
+	loss = (eps ** 2).sum();
+	#loss = epsilon.modulus();
+	ptn_grad = rv.pattern();
+	ptn_grad.initial();
+	rv.adjust(ptn_grad, sample, epsilon);
+	grad = ptn_grad.numpy().astype(np.float64).ravel() * 2;
+	return (loss, grad);
 
-print(epsilon.geth(), epsilon.getw())
-epsilon_save = rv.mat_f(epsilon);
-ptn_save = rv.pattern(ptn)
+#loss0, grad0 = fun(weight);
+#print("loss0: ", loss0);
+#epsilon = 1e-5;
 
-epsilon = rv.mat_f(epsilon_save);
-ptn = rv.pattern(ptn_save);
+#grad_delta = grad0.copy();
+#for i in range(10):
+#	weight[i] += epsilon;
+#	loss_delta = fun(weight)[0];
+#	print("loss_delta: ", loss_delta);
+#	grad_delta[i] = (loss_delta - loss0) / epsilon;
+#	weight[i] = 0;
 
-print("alpha: ",alpha);
-print("epsilon_2: ",epsilon_2);
+#print(grad0[:10], grad_delta[:10])
 
-for i in range(2000):
-	epsilon *= alpha;
-	rv.adjust(ptn,sample,epsilon);
+result = optimize.minimize(
+	fun, weight, method = "L-BFGS-B", jac = True,
+	options = {
+		"disp": True,
+		"maxcor": 25,
+		"maxiter": 400
+	}
+)
 
-	value = rv.evaluate(ptn,sample);
-	epsilon = target - value;
-	if i % 10 == 0:
-		epsilon_2 = epsilon.modulus();
-		print("i: ",i," epsilon_2: ",epsilon_2);
+print(result);
 
-grp = rv.group();
-grp.vec.append(ptn);
-grp.save(dir_save + "ptn_opt.dat");
+#value = rv.evaluate(ptn,sample);
+#epsilon = target - value;
+#epsilon_2 = epsilon.modulus();
+
+#print(epsilon.geth(), epsilon.getw())
+#epsilon_save = rv.mat_f(epsilon);
+#ptn_save = rv.pattern(ptn)
+
+#epsilon = rv.mat_f(epsilon_save);
+#ptn = rv.pattern(ptn_save);
+
+#print("alpha: ",alpha);
+#print("epsilon_2: ",epsilon_2);
+
+#for i in range(2000):
+#	epsilon *= alpha;
+#	rv.adjust(ptn,sample,epsilon);
+
+#	value = rv.evaluate(ptn,sample);
+#	epsilon = target - value;
+#	if i % 10 == 0:
+#		epsilon_2 = epsilon.modulus();
+#		print("i: ",i," epsilon_2: ",epsilon_2);
+
+#grp = rv.group();
+#grp.vec.append(ptn);
+#grp.save(dir_save + "ptn_opt.dat");
