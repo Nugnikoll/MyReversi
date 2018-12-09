d9ab19c86651d0cde25bc4f051bccd72032f81d8
Rick <1801210375@pku.edu.cn>
2018-12-10 00:21:51 +0800

    Use argument parser in pattern_optimize.py .
    Former-commit-id: 816f118121770f64e66a694823ab3c32353f9991

--- python/pattern_optimize.py
+++ python/pattern_optimize.py
@@ -1,17 +1,27 @@
-import reversi as rv;
 import time;
 import matplotlib.pyplot as plt;
 import os;
 import numpy as np;
 from scipy import optimize;
+import argparse
+import reversi as rv;
+
+parser = argparse.ArgumentParser(description = "Training parameters for reversi programming");
+parser.add_argument("--num-simulate", type = int, default = 10000, help = "number of random self-play games (default: 10000)");
+parser.add_argument("--path-sample", type = str, default = "./sample.dat", help = "path to find sample (default: ./sample.dat) sample will be generated if it cannot be found here");
+parser.add_argument("--path-occur", type = str, default = "./occurrence.dat", help = "path to find count of occurrence (default: ./occurrence.dat) sample will be generated if it cannot be found here");
+parser.add_argument("--path-target", type = str, default = "./target.dat", help = "path to find target of optimization (default: ./target) target will be generated if it cannot be found here");
+parser.add_argument("--path-save", type = str, default = "./pattern.dat", help = "path to save training result (default: ./pattern.dat)");
+parser.add_argument("--path-pattern", type = str, default = "", help = "generate target with old pattern and specify the path to find it");
+parser.add_argument("--epoch", type = int, default = 400, help = "The number of epochs to run (default: 400)");
+args = parser.parse_args();
 
 mthd = rv.mthd_ab | rv.mthd_kill | rv.mthd_pvs | rv.mthd_mtdf;
-dir_save = "./";
 
 rv.board.config();
 rv.pattern.config();
 
-name = ["sample.dat", "occurrence.dat"];
+name = [args.path_sample, args.path_occur];
 if os.path.exists(name[0]) and os.path.exists(name[1]):
 	print("sample already exists");
 	sample = rv.mat_brd();
@@ -19,11 +29,10 @@ if os.path.exists(name[0]) and os.path.exists(name[1]):
 	occurrence = rv.mat_i();
 	occurrence.load(name[1]);
 else:
-	size = int(input("Please input number of simulations: "));
 	print("generate sample");
 	time_begin = time.time();
 	occurrence = rv.mat_i();
-	sample = rv.sample_gen(size, occurrence);
+	sample = rv.sample_gen(args.num_simulate, occurrence);
 	time_end = time.time();
 	print("time: ", time_end - time_begin);
 	sample.save(name[0]);
@@ -32,17 +41,14 @@ else:
 size = sample.geth();
 print("sample size: ", size);
 
-name = "target.dat";
+name = args.path_target;
 if os.path.exists(name):
 	print("target already exists");
 	target = rv.mat_f();
 	target.load(name);
 else:
-	response = "";
-	while response != "y" and response != "n":
-		response = input("Whether generate target with old pattern? y/[n] ");
-	if response == "y":
-		rv.pattern.config("./pattern_balance.dat");
+	if os.path.exists(args.path_ptn):
+		rv.pattern.config(args.path_ptn);
 		mthd = mthd | rv.mthd_ptn;
 	print("generate target");
 	time_begin = time.time();
@@ -55,6 +61,8 @@ ptn_shape = rv.pattern().numpy().shape;
 weight = np.zeros(ptn_shape, dtype = np.float64).ravel();
 target_np = target.numpy();
 occurrence_np = occurrence.numpy() ** (3 / 4);
+target_np = np.exp(target_np / 10);
+target_np = (target_np / (target_np + 1) - 0.5) * 128;
 
 def fun(weight):
 	global size;
@@ -73,12 +81,12 @@ result = optimize.minimize(
 	options = {
 		"disp": True,
 		"maxcor": 25,
-		"maxiter": 400
+		"maxiter": args.epoch
 	}
 )
 time_end = time.time();
 print("time: ", time_end - time_begin);
 
-print(result);
 ptn = rv.pattern(result.x.astype(np.float32).reshape(ptn_shape));
-ptn.save("pattern.dat");
+ptn.balance();
+ptn.save(args.path_save);
